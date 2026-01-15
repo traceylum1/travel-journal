e2e78eb0f0c875373bce81d4d206644cac874fcd
Tracey Lum <traceylum@gmail.com>
2026-01-14 23:17:03 -0800

    add struct tags for database helper libraries, follow idiomatic go struct field naming

--- internal/models/models.go
+++ internal/models/models.go
@@ -4,65 +4,65 @@ import "time"
 
 // CreateUserInput represents the user input data sent to the backend to create the user
 type CreateUserInput struct {
-	Username    string  `json:"username"`
-    Password    string  `json:"password"`
+	Username    string  `json:"username" db:"username"`
+    Password    string  `json:"password" db:"username"`
 }
 
 // User represents the stored data about a user
 type User struct {
-    UserID      int         `json:"user_id"`
-	Username    string      `json:"username"`
-    Password    string      `json:"password"`
-	UserTrips   []int       `json:"user_trips"`
-    CreatedAt   time.Time   `json:"created_at"`
+    ID          int         `json:"user_id" db:"user_id"`
+	Username    string      `json:"username" db:"username"`
+    Password    string      `json:"password" db:"password"`
+	UserTrips   []int       `json:"user_trips" db:"user_trips"`
+    CreatedAt   time.Time   `json:"created_at" db:"created_at"`
 }
 
 // LoginRequest represents the user input data sent to the backend to validate login credentials
 type LoginRequest struct {
-	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	Username string `json:"username" db:"username" binding:"required"`
+	Password string `json:"password" db:"password" binding:"required"`
 }
 
 // CreateMarkerInput represents the user input data sent to the backend to create the marker
 type CreateMarkerInput struct {
-    TripID      int         `json:"trip_id"`
-    Location    string      `json:"location"`
-    Description string      `json:"description"`
-    Date        DateOnly    `json:"date"`
-    Latitude    float64     `json:"latitude"`
-    Longitude   float64     `json:"longitude"`
-    CreatedBy   string      `json:"created_by"`
+    TripID      int         `json:"trip_id" db:"trip_id"`
+    Location    string      `json:"location" db:"location"`
+    Description string      `json:"description" db:"description"`
+    Date        DateOnly    `json:"date" db:"date"`
+    Latitude    float64     `json:"latitude" db:"latitude"`
+    Longitude   float64     `json:"longitude" db:"longitude"`
+    CreatedBy   string      `json:"created_by" db:"created_by"`
 }
 
 // Marker represents the stored data about a marker that was placed on the map
 type Marker struct {
-    MarkerID    int         `json:"marker_id"`
-    TripID      int         `json:"trip_id"`
-    Location    string      `json:"location"`
-    Description string      `json:"description"`
-    Date        DateOnly    `json:"date"`
-    Latitude    float64     `json:"latitutde"`
-    Longitude   float64     `json:"longitude"`
-    CreatedBy   string      `json:"created_by"`
+    ID          int         `json:"marker_id" db:"marker_id"`
+    TripID      int         `json:"trip_id" db:"trip_id"`
+    Location    string      `json:"location" db:"location"`
+    Description string      `json:"description" db:"description"`
+    Date        DateOnly    `json:"date" db:"date"`
+    Latitude    float64     `json:"latitude" db:"latitude"`
+    Longitude   float64     `json:"longitude" db:"longitude"`
+    CreatedBy   string      `json:"created_by" db:"created_by"`
 }
 
 // CreateTripInput represents the user input data sent to the backend to create the marker
 type CreateTripInput struct {
-    TripName    string      `json:"trip_name"`
+    TripName    string      `json:"trip_name" db:"trip_name"`
     TripMarkers []int       `json:"trip_markers"`
-	StartDate   DateOnly	`json:"start_date"`
-    EndDate     DateOnly    `json:"end_date"`
-    Description string      `json:"description"`
-    CreatedBy   string      `json:"created_by"`
+	StartDate   DateOnly	`json:"start_date" db:"start_date"`
+    EndDate     DateOnly    `json:"end_date" db:"end_date"`
+    Description string      `json:"description" db:"description"`
+    CreatedBy   string      `json:"created_by" db:"created_by"`
 }
 
 // Trip represents the stored data about a trip that was created
 type Trip struct {
-    TripID      int         `json:"trip_id"`
-    TripName    string      `json:"trip_name"`
+    ID          int         `json:"trip_id" db:"trip_id"`
+    TripName    string      `json:"trip_name" db:"trip_name"`
     TripMarkers []int       `json:"trip_markers"`
-	StartDate   DateOnly	`json:"start_date"`
-    EndDate     DateOnly    `json:"end_date"`
-    Description string      `json:"description"`
-    CreatedBy   string      `json:"created_by"`
+	StartDate   DateOnly	`json:"start_date" db:"start_date"`
+    EndDate     DateOnly    `json:"end_date" db:"end_date"`
+    Description string      `json:"description" db:"description"`
+    CreatedBy   string      `json:"created_by" db:"created_by"`
 }
